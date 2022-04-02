d84cef2bd1abb479f446a591c6e24949931072ac
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-04-02 20:50:25 +0000

    Add font family

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function MyDocument() {
+  return (
+    <Html lang="en" className="h-full">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:wght@400;700&family=Public+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="h-full">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Ibarra Real Nova', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
