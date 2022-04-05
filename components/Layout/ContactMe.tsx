ff9c5e504ed6d4cae5f7c0a287d7d345e14a57be
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-04-05 21:21:12 +0000

    Add divider to contact me section

--- components/Layout/ContactMe.tsx
+++ components/Layout/ContactMe.tsx
@@ -2,10 +2,11 @@ import { LinkButton } from '@/components/Ui'
 
 function ContactMe() {
   return (
-    <section className="flex flex-col items-center md:flex-row md:justify-between">
-      <h2 className="mb-10 font-serif text-4xl font-bold leading-10 tracking-tight text-center md:text-left md:mb-0 w-80">
+    <section className="flex flex-col items-center md:flex-row md:justify-between md:space-x-8">
+      <h2 className="mb-10 w-80 font-serif text-4xl font-bold tracking-tight leading-10 text-center md:text-left md:mb-0">
         Interested in doing a project together?
       </h2>
+      <div className="hidden flex-1 border-b border-gray-300 md:block"></div>
       <LinkButton href="mailto:mohamed@elidrissi.dev" variant="secondary">
         Contact Me
       </LinkButton>
